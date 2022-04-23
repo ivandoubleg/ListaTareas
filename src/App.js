63be55e0924799049f4d04a6140d1847a3165d47
ivandoubleg <irvinivan96@gmail.com>
2022-04-22 23:33:33 -0500

    Creado del proyecto inicial

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Form from './components/Form';
+
+const  App = () => { //se actualizo por arrow funtion con base a ecs6
+
+  return (
+      <div className="App">
+        <div className="App-content">
+          
+          < Form />
+        </div>
+      </div>
+
+  );
+}
+export default App;
